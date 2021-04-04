8534895356f1b46db6a709738620dd830aa9ba20
mantaev-vfl <ali_mantaev@hotmail.com>
2021-04-03 23:54:14 -0400

    Update unittest_api.py

--- unittest_api.py
+++ unittest_api.py
@@ -7,5 +7,11 @@ class TestFlaskApp(unittest.TestCase):
         response = tester.get('/')
         self.assertEqual(response.status_code, 200)
 
+    def test_contentLength(self):
+        # The length of the content should be 2
+        tester = app.test_client(self)
+        response = tester.get("/")
+        print(response)
+
 if __name__ == '__main__':
     unittest.main()
